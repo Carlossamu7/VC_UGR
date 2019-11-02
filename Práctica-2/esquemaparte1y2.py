846a5b52cbc77f2d5dc6154aed433eb2053248b1
carlossamu7 <carlossamu7@gmail.com>
2019-11-02 16:25:06 +0100

    Nuevos avances

--- Práctica-2/esquemaparte1y2.py
+++ Práctica-2/esquemaparte1y2.py
@@ -4,31 +4,122 @@
 ############ CARGAR LAS LIBRERÍAS NECESARIAS ############################
 #########################################################################
 
-# A completar: esquema disponible en las diapositivas
+# Importar librerı́as necesarias
+import numpy as np
+import keras
+import matplotlib . pyplot as plt
+import keras . utils as np_utils
+# Importar modelos y capas que se van a usar
+
+# Importar el optimizador a usar
+from keras . optimizers import SGD
+# Importar el conjunto de datos
+from keras . datasets import cifar100
 
 #########################################################################
 ######## FUNCIÓN PARA CARGAR Y MODIFICAR EL CONJUNTO DE DATOS ###########
 #########################################################################
 
-# A completar: función disponible en las diapositivas
+#
+""" Sólo se le llama una vez. Devuelve 4 vectores conteniendo, por este orden, las imágenes de entrenamiento,
+    las clases de las imagenes de entrenamiento, las imágenes del conjunto de test y las clases del conjunto de test.
+"""
+def cargarImagenes():
+    # Cargamos Cifar100. Cada imagen tiene tamaño (32 , 32 , 3). Nos quedamos con las imágenes de 25 de las clases.
+    (x_train, y_train), (x_test, y_test) = cifar100.load_data(label_mode='fine')
+
+    x_train = x_train.astype('float32')
+    x_test = x_test.astype('float32')
+    x_train /= 255
+    x_test /= 255
+
+    train_idx = np.isin(y_train, np.arange (25))
+    train_idx = np.reshape(train_idx, -1)
+    x_train = x_train[train_idx]
+    y_train = y_train[train_idx]
+
+    test_idx = np.isin(y_test, np.arange (25))
+    test_idx = np.reshape(test_idx, -1)
+    x_test = x_test[test_idx]
+    y_test = y_test[test_idx]
+
+    # Transformamos los vectores de clases en matrices. Cada componente se convierte en un vector de
+    # ceros con un uno en la componente correspondiente a la clase a la que pertenece la imagen.
+    y_train = np_utils.to_categorical(y_train, 25)
+    y_test = np_utils.to_categorical(y_test, 25)
+
+    return x_train, y_train, x_test, y_test
+
 
 #########################################################################
 ######## FUNCIÓN PARA OBTENER EL ACCURACY DEL CONJUNTO DE TEST ##########
 #########################################################################
 
-# A completar: función disponible en las diapositivas
+""" Devuelve el accuracy de un modelo (porcentaje de etiquetas bien predichas frente al total).
+- labels: vector de etiquetas verdaderas.
+- preds: vector de etiquetas predichas.
+"""
+def calcularAccuracy (labels, preds):
+    labels = np.argmax(labels, axis = 1)
+    preds = np.argmax(preds, axis = 1)
+
+    accuracy = sum (labels == preds)/ len(labels)
+
+    return accuracy
 
 #########################################################################
 ## FUNCIÓN PARA PINTAR LA PÉRDIDA Y EL ACCURACY EN TRAIN Y VALIDACIÓN ###
 #########################################################################
 
-# A completar: función disponible en las diapositivas
+
+""" Pinta dos gráficas, una con la evolución de la función de pérdida en el conjunto de train y en el de validación,
+    y otra con la evolución del accuracy en el conjunto de train y el de validación.
+- hist: historial del entrenamiento del modelo (salida de fit () y fit_generator ()).
+"""
+def mostrarEvolucion(hist):
+    loss = hist.history ['loss']
+    val_loss = hist.history['val_loss']
+    plt.plot(loss)
+    plt.plot(val_loss)
+    plt.legend(['Training loss', 'Validation loss'])
+    plt.show()
+
+    acc = hist.history['acc']
+    val_acc = hist.history['val_acc']
+    plt.plot(acc)
+    plt.plot(val_acc )
+    plt.legend(['Training accuracy', 'Validation accuracy'])
+    plt.show()
 
 #########################################################################
 ################## DEFINICIÓN DEL MODELO BASENET ########################
 #########################################################################
 
-# A completar
+if K.image_data_format() == 'channels_first':
+    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+    input_shape = (1, img_rows, img_cols)
+else:
+    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+    input_shape = (img_rows, img_cols, 1)
+
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(5, 5), activation='relu', input_shape=input_shape))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
+
+model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adadelta(), metrics=['accuracy'])
+
+model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1, validation_data=(x_test, y_test))
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
 
 #########################################################################
 ######### DEFINICIÓN DEL OPTIMIZADOR Y COMPILACIÓN DEL MODELO ###########
